678fa8cb02537a0ccb81adbd0134c26f3c71572d
Akndju <davidd.akindoju@gmail.com>
2021-06-27 16:12:58 +0100

    integrated ability to send whatsapp msgs with twilio api

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,35 @@ import { useState } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [text, setText] = useState("");
-  const [isSuccessful, setIsSuccessful] = useState(false);
+  const [smsNumber, setSmsNumber] = useState("");
+  const [smsText, setSmsText] = useState("");
+  const [whatsappNumber, setWhatsappNumber] = useState("");
+  const [whatsappText, setWhatsappText] = useState("");
+  const [isSmsSuccessful, setIsSmsSuccessful] = useState(false);
+  const [isWhatsappSuccessful, setIsWhatsappSuccessful] = useState(false);
 
-  const sendText = () => {
+  const sendSMS = () => {
     axios
-      .post("http://localhost:5000/send", {
-        phoneNumber: phoneNumber,
-        text: text,
+      .post("http://localhost:5000/sms", {
+        smsNumber: smsNumber,
+        smsText: smsText,
       })
       .then(function (response) {
-        response === "Successful" && setIsSuccessful(true);
+        response === "Successful" && setIsSmsSuccessful(true);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const sendWhatsappMsg = () => {
+    axios
+      .post("http://localhost:5000/whatsapp", {
+        whatsappNumber: whatsappNumber,
+        whatsappText: whatsappText,
+      })
+      .then(function (response) {
+        response === "Successful" && setIsWhatsappSuccessful(true);
       })
       .catch(function (error) {
         console.log(error);
@@ -23,36 +40,73 @@ const App = () => {
 
   return (
     <div className="App">
-      <h2>Send SMS Message</h2>
-      <input
-        type="tel"
-        name="number"
-        placeholder="Enter Phone Number"
-        value={phoneNumber}
-        onChange={({ target }) => {
-          setPhoneNumber(target.value);
-        }}
-      />
-      <input
-        type="text"
-        name="msg"
-        placeholder="Enter Text Message"
-        value={text}
-        onChange={({ target }) => {
-          setText(target.value);
-        }}
-      />
-      <button
-        type="button"
-        className="button"
-        onClick={(event) => {
-          event.preventDefault();
-          sendText();
-        }}
-      >
-        Send Text Message
-      </button>
-      {isSuccessful && <p>Message sent to {phoneNumber}</p>}
+      {/* sms */}
+      <div>
+        <h2>Send SMS Message</h2>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Enter Recipient's Number"
+          value={smsNumber}
+          onChange={({ target }) => {
+            setSmsNumber(target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="msg"
+          placeholder="Enter Text Message"
+          value={smsText}
+          onChange={({ target }) => {
+            setSmsText(target.value);
+          }}
+        />
+        <button
+          type="button"
+          className="button-1"
+          onClick={(event) => {
+            event.preventDefault();
+            sendSMS();
+          }}
+        >
+          Send Message
+        </button>
+        {isSmsSuccessful && <p>Message sent to {smsNumber}</p>}
+      </div>
+
+      {/* whatsapp */}
+      <div>
+        <h2>Send Whatsapp Message</h2>
+        <input
+          type="tel"
+          name="number"
+          placeholder="Enter Recipient's Number"
+          value={whatsappNumber}
+          onChange={({ target }) => {
+            setWhatsappNumber(target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="msg"
+          placeholder="Enter Text Message"
+          value={whatsappText}
+          onChange={({ target }) => {
+            setWhatsappText(target.value);
+          }}
+        />
+        <button
+          type="button"
+          className="button-2"
+          onClick={(event) => {
+            event.preventDefault();
+            sendWhatsappMsg();
+          }}
+        >
+          Send Message
+        </button>
+        {isWhatsappSuccessful && <p>Message sent to {whatsappNumber}</p>}
+      </div>
     </div>
   );
 };
